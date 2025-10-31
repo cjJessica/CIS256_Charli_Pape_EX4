9ab2c1a93978f1a8591c89b5390f21ba8d59873d
Charli <cjpapetoo@gmail.com>
2025-10-31 12:23:49 -0700

    Choosing a word to guess from list randomly

--- guess_the_word.py
+++ guess_the_word.py
@@ -1,3 +1,13 @@
 #Charli Pape
 #CIS256 2025
 #EX 4
+
+#Import random library
+import random
+
+#Creation of list
+word_list = ["Potato", "Antarctica", "Treaty", "Penguins", "Isolation"]
+
+#Chosing a word to guess from list randomly
+word = random.choice(word_list)
+print(word)
