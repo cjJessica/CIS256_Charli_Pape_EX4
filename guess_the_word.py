103559038c4b0ce96588190aa634d9af38c98531
Charli <cjpapetoo@gmail.com>
2025-10-31 14:33:29 -0700

    Added finishing comments

--- guess_the_word.py
+++ guess_the_word.py
@@ -10,7 +10,6 @@ import random
 word_list = ["Potato", "Antarctica", "Treaty", "Penguins", "Isolation"]
 guess_list = []
 
-guess = ""
 
 # --Chosing a word to guess from list randomly and making sure its all lowercase
 word = (random.choice(word_list)).lower()
@@ -26,8 +25,11 @@ completed = False
 
 #Functions
 def user_guess(user_word_display, user_input):
+    """Checks to see if the user's input is in the word, if so it changes the displayed word's underscore to the actual letter via for in range. But if the input is not in the word, one life is lost"""
+    
     #Made num_of_guesses public so that function does not raise error
     global num_of_guesses
+    
     if user_input in word:
         for pos in range(length):
             if word[pos] == user_input:
@@ -41,6 +43,8 @@ def user_guess(user_word_display, user_input):
     
 
 def was_not_guessed(user_input):
+    """Checks to see if the user has already guessed the letter, if so function will return False. But if it's a new guess, the user's input will be added to the guess list and the function will return True"""
+    
     if user_input not in guess_list:
         guess_list.append(user_input)
         return True
@@ -61,15 +65,20 @@ while completed == False:
 
         #input
         user_input = input("\nPlease guess one letter: ")
+
+        #Runs was_not_guessed function and if true, user_guess function is ran. So if the user enters a new guess, the user_guess function will check if it is correct
         if was_not_guessed(user_input):
             user_guess(display_word, user_input)
 
+        #Checks if the display word list (when a string) is equal to 
         if "".join(display_word) == word:
             print("\n Congrats! You guessed the word!!!!")
             completed = True
             break
 
+    #Once user runs out of lives
     else:
+        #Program will end and output the word
         completed = True
         print(f"You failed, the word was {word}")
         break
